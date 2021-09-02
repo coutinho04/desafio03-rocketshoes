e79d19e480dca7d728f322111cb9515fd60bc29f
Felipe Coutinho <felipe04coutinho@gmail.com>
2021-09-02 02:26:44 -0300

    Challenge finished

---
+++ README.md
@@ -0,0 +1,106 @@
+<h1 align="center">
+  Rocketshoes
+</h1>
+
+<p align="center">
+  <a href="#-projeto">Projeto</a>&nbsp; • &nbsp;
+  <a href="#-tecnologias">Tecnologias</a>&nbsp; • &nbsp;
+  <a href="#-como-executar-o-projeto">Como executar</a>&nbsp; • &nbsp;
+  <a href="#-autor">Autor</a>&nbsp; • &nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<br>
+
+## 💻 Projeto
+
+<b>Rocketshoes</b> é uma aplicação desenvolvida para praticar a criação de um hook de carrinho de compras.  
+Com ela é possível:
+
+- Adicionar um novo produto ao carrinho
+- Remover um produto do carrinho
+- Alterar a quantidade de um produto no carrinho
+- Cálculo dos preços sub-total e total do carrinho
+- Validação de estoque
+- Exibição de mensagens de erro
+
+---
+
+## 🛠 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [React.js](https://pt-br.reactjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [React Router](https://reactrouter.com/)
+- [React Toastify](https://fkhadra.github.io/react-toastify/introduction)
+- [Styled Components](https://styled-components.com/)
+- [Polished](https://polished.js.org/)
+- [Axios](https://axios-http.com/)
+- [Node.js](https://nodejs.org/en/)
+
+---
+
+## 🚀 Como executar o projeto
+
+### Pré-requisitos
+
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas:
+[Git](https://git-scm.com), [Node.js](https://nodejs.org/en/).
+Além disto é bom ter um editor para trabalhar com o código como [VSCode](https://code.visualstudio.com/)
+
+### Rodando a aplicação
+
+```bash
+# Clone este repositório
+$ git clone https://github.com/coutinho04/desafio03-rocketshoes.git
+
+# Acesse a pasta do projeto no terminal/cmd
+$ cd desafio03-rocketshoes
+
+# Instale as dependências
+$ npm install
+# ou
+$ yarn
+
+# Execute a fake api com json server
+$ npm run server
+# ou
+$ yarn server
+
+# A api iniciará na porta:3333 - acesse <http://localhost:3333/stock> e <http://localhost:3333/products>
+
+# Execute a aplicação em modo de desenvolvimento
+$ npm run start
+# ou
+$ yarn start
+
+# A aplicação iniciará na porta:3000 - acesse <http://localhost:3000/>
+
+# Caso deseje rodar os testes utilize o comando
+$ npm test
+# ou
+$ yarn test
+
+```
+
+---
+
+### 👨‍💻 Autor
+
+ <img style="border-radius: 50%;" src="https://github.com/coutinho04.png" width="100px;" alt=""/>
+ <br />
+ <sub><b>Felipe Coutinho</b></sub></a>
+
+[![Linkedin Badge](https://img.shields.io/badge/-Felipe-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/felipecoutinho04/)](https://www.linkedin.com/in/felipecoutinho04/)
+[![Gmail Badge](https://img.shields.io/badge/-felipe04coutinho@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:felipe04coutinho@gmail.com)](mailto:felipe04coutinho@gmail.com)
+
+---
+
+## 📝 Licença
+
+Este projeto esta sobe a licença [MIT](./LICENSE).
+
+Feito com ❤️ por Felipe Coutinho 👋🏽 [Entre em contato!](https://www.linkedin.com/in/felipecoutinho04/)
+
+---
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,49 +23,92 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = cart.map((product) => ({ ...product }));
+      const productExists = updatedCart.find((product) => product.id === productId);
+      const currentAmount = productExists ? productExists.amount : 0;
+      const stock = await api.get<Stock>(`/stock/${productId}`);
+      const stockAmount = stock.data.amount;
+      const amount = currentAmount + 1;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      if (productExists) {
+        productExists.amount = amount;
+      } else {
+        const product = await api.get<Product>(`/products/${productId}`);
+        const newProduct = {
+          ...product.data,
+          amount: 1,
+        };
+        updatedCart.push(newProduct);
+      }
+
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = cart.map((product) => ({ ...product }));
+      const productIndex = updatedCart.findIndex((product) => product.id === productId);
+
+      if (productIndex >= 0) {
+        updatedCart.splice(productIndex, 1);
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+      } else {
+        throw Error();
+      }
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
-  const updateProductAmount = async ({
-    productId,
-    amount,
-  }: UpdateProductAmount) => {
+  const updateProductAmount = async ({ productId, amount }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) {
+        return;
+      }
+      const newAmount = amount;
+      const stock = await api.get<Stock>(`/stock/${productId}`);
+      const stockAmount = stock.data.amount;
+      if (newAmount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      const updatedCart = cart.map((product) => ({ ...product }));
+      const productExists = updatedCart.find((product) => product.id === productId);
+      if (productExists) {
+        productExists.amount = newAmount;
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+      } else {
+        throw Error();
+      }
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
-  return (
-    <CartContext.Provider
-      value={{ cart, addProduct, removeProduct, updateProductAmount }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
+  return <CartContext.Provider value={{ cart, addProduct, removeProduct, updateProductAmount }}>{children}</CartContext.Provider>;
 }
 
 export function useCart(): CartContextData {
